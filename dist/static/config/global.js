a5d1ff9c90a5d60a8ec4e13f938fac7c5716c26e
wanghaojie124 <593164063@qq.com>
2019-12-25 16:33:03 +0800

    12-25

--- dist/static/config/global.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = 'https://vkcampus.cdut.top';
-// const BASE_URL = 'http://129.204.61.233:2000';
-export default {
-    BASE_URL
-}
--- static/config/global.js
+++ static/config/global.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://vkcampus.cdut.top';
-// const BASE_URL = 'http://129.204.61.233:2000';
+// const BASE_URL = 'http://localhost:2000';
 export default {
     BASE_URL
 }
